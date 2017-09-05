efb6902d31e07ead6c3735938ea202955823dfc8
Robert <robertdibejar@gmail.com>
2017-09-05 19:58:51 -0300

    ya se pueden ver las carreras en la vista races

--- run/resources/views/race/races.blade.php
+++ run/resources/views/race/races.blade.php
@@ -18,4 +18,27 @@
 
       @endforeach
     </div>
+
+    <section class="item-map">
+      <div id="map">
+
+      </div>
+    </section>
+
+@endsection
+
+
+@section('js')
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script
+    async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCv2ct01-Nd8vzgRYN2pnUNbMsi2Rfa6Bg&libraries=places"
+  >
+  </script>
+  <script
+    async defer
+    type="text/javascript"
+    src="{{asset('js/gmaps.js')}}"
+  >
+  </script>
 @endsection
